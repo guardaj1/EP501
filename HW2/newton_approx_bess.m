24c3a4f7a875197b13eeab793f21567be191c92f
Julio Guardado <guardadojulio@hotmail.com>
2019-10-14 20:39:13 -0400

    hw2 update

--- HW2/newton_approx_bess.m
+++ HW2/newton_approx_bess.m
@@ -1,4 +1,4 @@
-function [root,it,success]=newton_approx(f,x0,eta,maxit,tol,verbose)
+function [root,it,success]=newton_approx_bess(x0,eta,maxit,tol,verbose)
 
 % root=newton_exact(f,fprime)
 %
@@ -6,34 +6,41 @@ function [root,it,success]=newton_approx(f,x0,eta,maxit,tol,verbose)
 % given a function which computes the derivative
 
 %% Error checking of input
-narginchk(2,6);   %check for correct number of inputs to function
-if (nargin<3)
+narginchk(1,5);   %check for correct number of inputs to function
+if (nargin<2)
     eta = .01;
 end
-if (nargin<4)
+if (nargin<3)
     maxit=100;       %maximum number of iterations allowed
 end %if
-if (nargin<5)
+if (nargin<4)
     tol=1e-6;        %how close to zero we need to get to cease iterations
 end %if
-if (nargin<6)
+if (nargin<5)
     verbose=false;
+end %if
+
+%% Make sure we don't start at an inflection point with zero derivative
+derivative = (besselj(0,x0+eta)-besselj(0,x0))/eta;
+if (abs(derivative)<tol)
+    warning(' Attempting to start Newton iterations near an inflection point, you may wish to restart with a different guess...');
+    x0=x0+1;   %bump the guess a ways off of initial value to see if we can get anything sensible
 end %if
 
 %% Newton iterations
 it=1;
 root=x0;
-fval=f(root);
+fval=besselj(0,root);
 converged=false;
 while(~converged && it<=maxit)
-    derivative = (f(root+eta)-f(root))/eta;
+    derivative = (besselj(0,root+eta)-besselj(0,root))/eta;
     if (abs(derivative)<100*tol)                %this (inflection point) will end up kicking the root really far away...
         converged=false;
         warning(' Derivative close to zero, terminating iterations with failed convergence... ');
         break;
     else
         root=root-fval./derivative;             % update root estimate
-        fval=f(root);                           % see how far off we are from zero...
+        fval=besselj(0,root);                   % see how far off we are from zero...
         if (verbose)
             fprintf(' iteration: %d; root:  %f + %f i; function value: %f, derivative:  %f \n',it,real(root),imag(root),fval,derivative);
         end %if
